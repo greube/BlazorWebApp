ffe9bb607ead390f3584a6d19365ca7c65c4e450
BuildTools <unconfigured@null.spigotmc.org>
2021-08-12 13:57:02 +1000

    Add project files.

---
+++ ReubenClarke/Models/DisplayPersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReubenClarke.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(50,ErrorMessage = "Name can't exceed 50 characters")]
+        public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name can't exceed 50 characters")]
+        public string LastName { get; set; }
+
+    }
+}
